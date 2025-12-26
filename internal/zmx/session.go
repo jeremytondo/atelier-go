d2415bff2789902a5b0315e23e22856f1b6c93cc
Jeremy Margaritondo <jmargari@gmail.com>
2025-12-26 16:04:05 -0600

    zmx fix.

--- internal/zmx/session.go
+++ internal/zmx/session.go
@@ -10,7 +10,9 @@ import (
 // It sets the working directory to dir for new sessions.
 // It connects the current process's Stdin, Stdout, and Stderr to the zmx command.
 func Attach(name string, dir string) error {
-	cmd := exec.Command("zmx", "attach", "-c", name)
+	// zmx attach <name> [command...]
+	// We omit the command to let zmx default to the user's shell.
+	cmd := exec.Command("zmx", "attach", name)
 	cmd.Dir = dir
 	cmd.Stdin = os.Stdin
 	cmd.Stdout = os.Stdout
